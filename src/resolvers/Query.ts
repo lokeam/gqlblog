53a2b6319553748047a41d703390f6c0f17ef832
Ahn Ming Loke <lokeahnming@gmail.com>
2023-12-29 17:14:52 -0500

    feat: add self query to get data about current user

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,15 @@
 import { Context } from "..";
 
 export const Query = {
+  self: (_: any, __: any, { userInformation, prisma }: Context) => {
+    if (!userInformation) return null;
+
+    return prisma.user.findUnique({
+      where: {
+        id: userInformation.userId
+      }
+    })
+  },
   posts: (_: any, __: any, { prisma }: Context) => {
     // Sort from most to least recent
     return prisma.post.findMany({
