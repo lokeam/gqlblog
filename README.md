b69d8e2e7a1d05c2b4e333860d750e6537380bc4
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-11 17:53:58 -0500

    feat: update docs and adjust file pathing

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-A simple React + Node + Apollo Server GraphQL Social Media (blog) application built from scratch. GraphQL API that uses Prisma v3 to connect to a Postgres database. Associated client-side application built with React.js.
+# GraphQL Blog
+
+A simple TypeScript, Node, GraphQL Apollo Server Social Media (blog) application built from scratch. Users may log into application via an external portal, authenticate via json web tokens + password hashing, then create, update and delete posts that may be displayed to non-logged in users. Clientside rendering served via React.
+
+## Notes
+- GraphQL API based on Shopify's Internal GraphQL API Design best practices public git repo [here](https://github.com/Shopify/graphql-design-tutorial/blob/master/TUTORIAL.md).
+- Uses [Prisma v3](https://www.prisma.io/docs/orm/more/upgrade-guides/upgrading-versions/upgrading-to-prisma-3) to connect to an externally hosted [Render](https://render.com/) Postgres database.
+- Associated client-side application built with React.js.
+
+## Usage
+
+### Starting the Server
+Navigate to `/server` directory. Run `npm start:dev`.
+
+Runs the Apollo Playground in the development mode.<br />
+Open [http://localhost:4000](http://localhost:4000) to view it in the browser.
+
+
+### Starting the Client - `npm start:dev`
+Navigate to `/client` directory. Run `npm start`.
+
+Boots up React clientside application.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ client/src/pages/Signup/Signup.js
@@ -0,0 +1,62 @@
+import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleClick = () => {};
+
+
+
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder=""
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Bio</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+          />
+        </Form.Group>
+        {error && <p>{error}</p>}
+        <Button onClick={handleClick}>Signup</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
